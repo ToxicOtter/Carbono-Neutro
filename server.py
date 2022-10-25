d55a332ede949ec8ede7ed805a9f68fe06eebae7
ToxicOtter <luan.lima.lopes16@gmail.com>
2022-10-25 11:14:50 -0300

    Fixação da leitura do KML e calculo de área

--- server.py
+++ server.py
@@ -1,74 +1,41 @@
 import os
 import geopandas as gpd
 from shapely.geometry import shape, Polygon
-from flask import Flask, render_template, request #importa a classe Flask
+from shapely import wkt
+from flask import Flask, render_template, request
 from fiona.drvsupport import supported_drivers
+from pyproj import Geod
 
 
 app = Flask(__name__) #criação de uma instancia da classe, o nome é um indicativo para o Python olhar em busca de arquivos
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] - False
-#db = SQLAlchemy(app)
 
-#class Upload(db.Model):
-#    id = db.Column(db.Integer, primary_key=True)
-#    filename = 
-
-app.config['UPLOAD_FOLDER'] = r"C:\Users\luan_\Documents\Luan\Programação\APS\2S\Carbono_Neutro\arquivo"
+app.config['UPLOAD_FOLDER'] = r"/home/carbonoFree/.virtualenvs/my-virtualenv/site/files"
 
 
-@app.route("/", methods=["GET","POST"]) #usado para mostrar ao Flask qual URL vai dar gatilho na função
+@app.route("/", methods=["GET","POST"])
 def main():
-    text = 0
+    #Recebe o arquivo KML do usuario, calcula a area e retorna como parametro na renderização do HTML
+    text=0
+
     if (request.method == "POST"):
         file = request.files['file']
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], "outro_teste.kml"))
 
-        #dom = parse(r'C:\Users\luan_\Documents\Luan\Programação\APS\2S\Carbono_Neutro\arquivo\Area teste.kml')
-        #entries = []
-        #placemarks = dom.getElementsByTagName("Placemark")
-
-        #def subfolders(node):
-        #    if node.parentNode == dom.documentElement:
-        #        return ""
-        #    else:
-        #        foldername = node.getElementsByTagName("name")[0].firstChild.data
-        #        path = subfolders(node.parentNode) + "/" + foldername
-        #        return path
-
-        #for i in placemarks:
-        #    longitude = i.getElementsByTagName("longitude")[0].firstChild.data
-        #    latitude = i.getElementsByTagName("latitude")[0].firstChild.data
-        #    coordinates = i.getElementsByTagName("coordinates")[0].firstChild.data
-        #    try:
-        #        name = i.getElementsByTagName("name")[0].firstChild.data
-        #    except:
-        #        name = ""
-        #    parent = i.parentNode
-        #    foldername = parent.getElementsByTagName("name")[0].firstChild.data
-        #    path = subfolders(parent) 
-        #    entries.append((name, latitude, longitude, coordinates, foldername, path)) # List of tuples
-
-        #df = pd.DataFrame(entries, columns=('name', 'latitude', 'longitude', 'coordinates', 'folder', 'path'))
-        #gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude, crs="EPSG:4326"))
-        #gdf = gpd.GeoDataFrame(df, geometry=Polygon(df.coordinates[0]), crs="EPSG:4326")
-        #print(f"coordinates: {df['coordinates'][0]}")
-
-
-
-
-
-        #poligono = gpd.read_file(r"C:\Users\luan_\Documents\Luan\Programação\APS\2S\Carbono_Neutro\arquivo\Area_teste-polygon.shp")
-        #print(f"poligono: {poligono.geometry[0]}")
-        #print(shape(poligono.loc[0,'geometry']).area)
-        
-
 
         #gpd.io.file.fiona.drvsupport.supported_drivers['KML'] = 'rw'
         supported_drivers['KML'] = 'rw'
-        my_map = gpd.read_file(r'C:\Users\luan_\Documents\Luan\Programação\APS\2S\Carbono_Neutro\arquivo\outro_teste.kml', driver='KML')
-        #print(shape(my_map.loc[0,'geometry']).area)
-        text = shape(my_map.loc[0,'geometry']).area
+        my_map = gpd.read_file(r'/home/carbonoFree/.virtualenvs/my-virtualenv/site/files/outro_teste.kml', driver='KML')
+        #text = shape(my_map.loc[0,'geometry']).area
 
-    print(text)
+
+        geod = Geod(ellps="WGS84")
+        poly = wkt.loads(str(my_map.loc[0,'geometry']))
+        area = abs(geod.geometry_area_perimeter(poly)[0])
+        #text = (area * 2.353)/10000
+        text = area
+        #https://stackoverflow.com/questions/23697374/calculate-polygon-area-in-planar-units-e-g-square-meters-in-shapely
+
     return render_template("index.html", text=text)
+
+if __name__ == '__main__':
+    app.run()
